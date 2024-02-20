f63e4af133ecb96a33dd5a51eb29540ebd103234
Virginia Leticia Afonso <vlafonso@hotmail.com>
2024-02-20 15:16:21 -0300

    Add aula 3 - exercicio 1 - ze do lanche

---
+++ Aula_3/Exercicio1_ZeDoLanche/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int lanche1 = 10;
+        int lanche2 = 4;
+        int lanche3 = 2;
+
+        int total = lanche1+lanche2+lanche3;
+        float media = total/3.0f;
+        
+        System.out.println("O total de lanches foi: "+total);
+        System.out.println("A media de lanches foi: "+media);
+    }
+}
