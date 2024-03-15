f4dff1b87546358f9ad323026fa97967bbb686c9
Virginia Leticia Afonso <vlafonso@hotmail.com>
2024-03-15 14:12:12 -0300

    Add anotações de aula - Arrays

--- Aula_5/Arrays/src/Exercicio0/Cantina.java
+++ Aula_5/Arrays/src/Exercicio0/Cantina.java
@@ -5,18 +5,27 @@ public class Cantina {
     Salgado[] salgados = new Salgado[20];
     int quantidade = 0;
 
+    /*
     void addSalgado(Salgado novoSalgado){
         this.salgados[this.quantidade] = novoSalgado;
         this.quantidade += 1;
+    }*/
+
+    //Método visto na aula
+    void addSalgado(Salgado novoSalgado){
+        for(int i=0; i < salgados.length; i++){
+            if(salgados[i] == null){
+                salgados[i] = novoSalgado;
+                break;
+            }
+        }
     }
 
     void mostraInfo(){
         System.out.println("O nome da cantina é: "+this.nome);
         System.out.println("Ela possui os salgados:");
         for(Salgado salgado : salgados){
-            if(salgado == null)
-                break;
-            else
+            if(salgado != null)
                 System.out.println(" - "+salgado.nome);
         }
     }
